d30419586248083aef5d578d5ff7fc7b4a7f628f
Nagasai <nagasai36912@gmail.com>
2026-07-17 14:35:57 +0530

    added support for pdf files in the extraction step

--- src/backend/extract.py
+++ src/backend/extract.py
@@ -1,18 +1,10 @@
 import base64
 import sys
 from pathlib import Path
-
 from dotenv import load_dotenv
 from openai import OpenAI
 from pydantic import BaseModel
-from rich import print as rprint
-
-MODEL = "gpt-5-mini"
-EXTRACTION_PROMPT = """
-You are expert at structured data extraction.
-You are given a medical report of a user's blood test.
-Extract only the meaningful information necessary for downstream tasks as required.
-""".replace("\n", "")
+import pymupdf
 
 
 class NominalRange(BaseModel):
@@ -32,28 +24,30 @@ class Measurements(BaseModel):
     collection: list[Measurement]
 
 
+MODEL = "gpt-5-mini"
+EXTRACTION_PROMPT = """
+You are expert at structured data extraction.
+You are given a medical report of a user's blood test.
+Extract only the meaningful information necessary for downstream tasks as required.
+""".replace("\n", "")
 
+load_dotenv()
+client = OpenAI()
 
-def encode_img_base64(imgpath: Path):
-    raw_bytes = imgpath.read_bytes()
-    encoded_bytes = base64.b64encode(raw_bytes)
+def encode_bytes_base64(imgbytes: bytes):
+    encoded_bytes = base64.b64encode(imgbytes)
     return encoded_bytes.decode("utf-8")
 
-if __name__ == "__main__":
-    load_dotenv()
-    client = OpenAI()
-
-    imgpath = Path(sys.argv[1])
-    img_base64 = encode_img_base64(imgpath)
-    img_ext = "png"
+def get_data_from_imgbytes(imgbytes: bytes, img_ext: str) -> Measurements | None:
+    img_base64 = encode_bytes_base64(imgbytes)
     response = client.responses.parse(
         model=MODEL,
         input=[
             {"role": "system", "content": EXTRACTION_PROMPT},
-            {
+            {  # pyright: ignore[reportArgumentType]
                 "role": "user",
                 "content": [
-                    {"type": "input_text", "text": "Extract from this image"},
+                    {"type": "input_text", "text": "Extract data from this image"},
                     {
                         "type": "input_image",
                         "image_url": f"data:image/{img_ext};base64,{img_base64}",
@@ -63,4 +57,34 @@ if __name__ == "__main__":
         ],
         text_format=Measurements,
     )
-    rprint(response)
+
+    return response.output_parsed
+
+if __name__ == "__main__":
+    from rich import print as rprint
+
+    filepath = Path(sys.argv[1])
+    img_ext = 'png'
+    if filepath.suffix == '.pdf':
+        doc = pymupdf.open(filepath)
+        imgbytes_container: list[bytes] = []
+        for page in doc:
+            pix = page.get_pixmap()
+            bytes = pix.tobytes(img_ext)
+            imgbytes_container.append(bytes)
+
+    # filepath.suffix.lower() gives us extension, prepended with '.'
+    elif (img_ext := filepath.suffix.lower()[1:]) in ['jpeg', 'png', 'jpg']:
+        imgbytes_container = [filepath.read_bytes()]
+
+    else:
+        print("Unsupported filetype")
+        exit(1)
+
+    extracted_data = Measurements(collection=[])
+    for imgbytes in imgbytes_container:
+        extracted_page_data = get_data_from_imgbytes(imgbytes, img_ext)
+        if extracted_page_data is not None:
+            extracted_data.collection.extend(extracted_page_data.collection)
+
+    rprint(extracted_data)
